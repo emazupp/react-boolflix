137f499c5f46dd0287452a87616baee636c547da
emazupp <zuppardo.emanuele2001@gmail.com>
2024-12-16 14:19:15 +0100

    env config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  console.log(import.meta.env);
+  return (
+    <>
+      <p>api key: {import.meta.env.VITE_TEST_VARIABLE}</p>
+    </>
+  );
 }
 
 export default App;
